7137cfca062ead8abf6497bf9702321c598ad3fc
Syrnnik <sakh.alexandr@gmail.com>
2023-10-23 17:34:46 +0300

    feat: updating project func

---
+++ integrations/firebase/firestore_update_project.py
@@ -0,0 +1,52 @@
+import requests
+
+# def update_project_status_and_translated_link_by_id(
+#     projectId: str,
+#     status: str,
+#     translated_file_link: str | None
+# ):
+#     try:
+#         project_ref = db.collection(PROJECT_COLLECTION).document(projectId)
+#         projectFieldsToUpdate = {
+#             "status": status
+#         }
+
+#         if not (translated_file_link == None):
+#             projectFieldsToUpdate.update({
+#                 "translatedFileLink": translated_file_link
+#             })
+
+#         project_ref.update(projectFieldsToUpdate)
+#     except Exception as e:
+#         print(f"An error occurred: {str(e)}")
+#         raise updating_project_exception
+
+updating_project_exception = Exception(
+    "Error while updating project"
+)
+
+UPDATE_PROJECT_URL = "https://us-central1-audioland-dub.cloudfunctions.net/updateTranslatedProject"
+
+
+def update_project_status_and_translated_link_by_id(
+    project_id: str,
+    status: str,
+    translated_file_link: str | None
+):
+    try:
+        project_fields_to_update = {
+            "id": project_id,
+            "status": status,
+            "translatedFileLink": translated_file_link
+        }
+
+        response = requests.post(
+            UPDATE_PROJECT_URL,
+            project_fields_to_update,
+        )
+        if not response.ok:
+            raise updating_project_exception
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        raise updating_project_exception
