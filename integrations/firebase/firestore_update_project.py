4caa5a41760b5f3e105debde80d89ff9766b3bf2
Syrnnik <sakh.alexandr@gmail.com>
2023-10-29 01:27:22 +0300

    style: remove some comments (test commit)

--- integrations/firebase/firestore_update_project.py
+++ integrations/firebase/firestore_update_project.py
@@ -3,27 +3,6 @@ import requests
 from config.config import UPDATE_PROJECT_URL
 from config.logger import catch_error
 
-# def update_project_status_and_translated_link_by_id(
-#     projectId: str,
-#     status: str,
-#     translated_file_link: str | None
-# ):
-#     try:
-#         project_ref = db.collection(PROJECT_COLLECTION).document(projectId)
-#         projectFieldsToUpdate = {
-#             "status": status
-#         }
-
-#         if not (translated_file_link == None):
-#             projectFieldsToUpdate.update({
-#                 "translatedFileLink": translated_file_link
-#             })
-
-#         project_ref.update(projectFieldsToUpdate)
-#     except Exception as e:
-#         print(f"An error occurred: {str(e)}")
-#         raise updating_project_exception
-
 updating_project_exception = Exception(
     "Error while updating project"
 )
