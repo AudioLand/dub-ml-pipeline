07f7633b6e409815b966cf40263f794730ca25ed
Syrnnik <sakh.alexandr@gmail.com>
2023-10-29 00:38:46 +0300

    style: clean code (test commit)

--- integrations/firebase/init_firebase.py
+++ integrations/firebase/init_firebase.py
@@ -7,7 +7,3 @@ from config.config import CERTIFICATE_PATH
 def init_firebase():
     cred = credentials.Certificate(CERTIFICATE_PATH)
     firebase_admin.initialize_app(cred)
-
-# db = firestore.client()
-
-# PROJECT_COLLECTION = "projects"
