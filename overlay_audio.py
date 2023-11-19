dc4693d8b84c118b2ac32700b2e731dfe9a19131
Syrnnik <sakh.alexandr@gmail.com>
2023-11-19 20:25:33 +0300

    refactor something

--- overlay_audio.py
+++ overlay_audio.py
@@ -1,7 +1,7 @@
+import os
 from pathlib import Path
 
-# from ffmpeg.video import playback_speed
-from moviepy.editor import *
+from moviepy.editor import VideoFileClip, AudioFileClip
 from pydub import AudioSegment
 
 from config.logger import catch_error
@@ -77,12 +77,12 @@ def overlay_audio(
                 print(f"(overlay_audio) Video segment duration: {video_duration:.2f}ms | {video_duration / 1000:.2f}s")
                 print(f"(overlay_audio) Audio segment duration: {audio_duration:.2f}ms | {audio_duration / 1000:.2f}s")
 
-                # Speed up audio if it's need
-                # if audio_duration - video_duration > 0.5:
-                #     ratio = audio_duration / video_duration
-                #     audio_segment = audio_segment.speedup(playback_speed=ratio)
-                #     if show_logs:
-                #         print(f"(overlay_audio) Speeding up audio by a factor of: {ratio:.2f}")
+            # Speed up audio if it's need
+            # if audio_duration - video_duration > 0.5:
+            #     ratio = audio_duration / video_duration
+            #     audio_segment = audio_segment.speedup(playback_speed=ratio)
+            #     if show_logs:
+            #         print(f"(overlay_audio) Speeding up audio by a factor of: {ratio:.2f}")
 
             final_audio = final_audio.overlay(audio_segment, position=video_start_time * 1000)
             if show_logs:
@@ -109,6 +109,7 @@ def overlay_audio(
         # Close the clips to free up memory
         video.close()
         audio.close()
+        # Remove audio overlay file
         os.remove(overlay_audio_name)
 
         print(f"(overlay_audio) Translated video saved, path: {translated_video_path}")
@@ -126,97 +127,45 @@ def overlay_audio(
 # For local test
 if __name__ == "__main__":
     sample_text_segments = [
-        {
-            'timestamp': [0.0, 4.52],
-            'text': 'Я просыпаюсь утром и хочу дотянуться до своего телефона, но я знаю, что даже если я',
-            'audio_timestamp': [2985, 8959]
-        },
-        {
-            'timestamp': [4.52, 9.8],
-            'text': 'увеличу яркость экрана, это все равно не будет достаточно ярко, чтобы вызвать скачок кортизола',
-            'audio_timestamp': [11582, 17909]
-        },
-        {
-            'timestamp': [9.8, 15.42],
-            'text': 'и для того, чтобы я был максимально бодрым и сосредоточенным в течение дня и оптимизировал свой',
-            'audio_timestamp': [20576, 26696]
-        },
-        {
-            'timestamp': [15.42, 16.42],
-            'text': 'сон ночью.',
-            'audio_timestamp': [29366, 30503]
-        },
-        {
-            'timestamp': [16.42, 23.76],
-            'text': 'Поэтому я встаю из кровати и выхожу на улицу, и если день светлый и ясный, а',
-            'audio_timestamp': [34132, 39918]
-        },
-        {
-            'timestamp': [23.76, 26.08],
-            'text': 'солнце низко в небе, или солнце,',
-            'audio_timestamp': [42570, 45069]
-        },
-        {
-            'timestamp': [26.08, 27.2],
-            'text': ' знаете, начинает подниматься над головой,',
-            'audio_timestamp': [47868, 50767]
-        },
-        {
-            'timestamp': [27.2, 28.72],
-            'text': ' то, что мы называем низким солнечным углом,',
-            'audio_timestamp': [53412, 56276]
-        },
-        {
-            'timestamp': [28.72, 31.76],
-            'text': ' и я знаю, что выхожу на улицу в нужное время.',
-            'audio_timestamp': [58982, 62149]
-        },
-        {
-            'timestamp': [31.76, 34.8],
-            'text': 'Если облачно, и я не вижу солнца,',
-            'audio_timestamp': [65830, 68568]
-        },
-        {
-            'timestamp': [34.8, 36.4],
-            'text': 'я также знаю, что делаю правильное дело',
-            'audio_timestamp': [71331, 74083]
-        },
-        {
-            'timestamp': [36.4, 38.56],
-            'text': ' потому что оказывается, особенно в облачные дни,',
-            'audio_timestamp': [76748, 80054]
-        },
-        {
-            'timestamp': [38.56, 40.64],
-            'text': ' вы хотите выйти на улицу и получить как можно больше световой энергии',
-            'audio_timestamp': [82773, 87412]
-        },
-        {
-            'timestamp': [40.64, 42.4],
-            'text': ' или фотонов в ваши глаза.',
-            'audio_timestamp': [90171, 92258]
-        },
-        {
-            'timestamp': [42.4, 44.32],
-            'text': 'Но допустим, день очень ясный,',
-            'audio_timestamp': [95874, 98348]
-        },
-        {
-            'timestamp': [44.32, 45.36],
-            'text': ' и я вижу, где находится',
-            'audio_timestamp': [101133, 103199]
-        },
-        {
-            'timestamp': [45.36, 52.08],
-            'text': ' солнце. Мне не нужно смотреть прямо на солнце. Если оно очень низко в небе, я могу сделать это',
-            'audio_timestamp': [105861, 106748]
-        },
-        {
-            'timestamp': [52.08, 56.48],
-            'text': ' потому что это не будет очень больно для моих глаз. Однако, если солнце немного ярче,',
-            'audio_timestamp': [107394, 110020]
-        }
-    ]
+        {'timestamp': [0.0, 3.36], 'text': ' I wake up in the morning and I want to reach for my phone,',
+         'audio_timestamp': [2948, 5655]},
+        {'timestamp': [3.36, 5.74], 'text': ' but I know that even if I were to crank up the brightness',
+         'audio_timestamp': [8473, 11348]},
+        {'timestamp': [5.74, 7.0], 'text': ' on that phone screen,', 'audio_timestamp': [13964, 15317]},
+        {'timestamp': [7.0, 10.28], 'text': " it's not bright enough to trigger that cortisol spike.",
+         'audio_timestamp': [18068, 20836]},
+        {'timestamp': [10.28, 14.36], 'text': ' And for me to be at my most alert and focused throughout',
+         'audio_timestamp': [24636, 27597]},
+        {'timestamp': [14.36, 16.16], 'text': ' the day and to optimize my sleep at night.',
+         'audio_timestamp': [30339, 32738]},
+        {'timestamp': [16.16, 20.2], 'text': ' So what I do is I get out of bed and I go outside.',
+         'audio_timestamp': [36478, 39279]},
+        {'timestamp': [20.2, 23.34], 'text': " And if it's a bright, clear day,", 'audio_timestamp': [43058, 44169]},
+        {'timestamp': [23.34, 25.18], 'text': ' and the sun is low in the sky,', 'audio_timestamp': [44157, 45038]},
+        {'timestamp': [25.18, 27.18], 'text': ' or the sun is starting to get overhead,',
+         'audio_timestamp': [47833, 49620]},
+        {'timestamp': [27.18, 28.7], 'text': ' what we call low solar angle,', 'audio_timestamp': [52382, 54371]},
+        {'timestamp': [28.7, 31.74], 'text': " then I know I'm getting outside at the right time.",
+         'audio_timestamp': [57195, 59113]},
+        {'timestamp': [31.74, 34.78], 'text': " If there's cloud cover and I can't see the sun,",
+         'audio_timestamp': [61920, 64125]},
+        {'timestamp': [34.78, 36.38], 'text': " I also know I'm doing a good thing,",
+         'audio_timestamp': [67939, 70579]},
+        {'timestamp': [36.38, 38.56], 'text': ' because it turns out, especially on cloudy days,',
+         'audio_timestamp': [73378, 75020]},
+        {'timestamp': [38.56, 40.66], 'text': ' you want to get outside and get as much light energy',
+         'audio_timestamp': [77931, 79233]},
+        {'timestamp': [40.66, 42.42], 'text': ' or photons in your eyes.', 'audio_timestamp': [79206, 81228]},
+        {'timestamp': [42.42, 44.3], 'text': " But let's say it's a very clear day", 'audio_timestamp': [83858, 86576]},
+        {'timestamp': [44.3, 46.44], 'text': ' and I can see where the sun is.', 'audio_timestamp': [89346, 91236]},
+        {'timestamp': [46.44, 49.24], 'text': ' I do not need to stare directly into the sun.',
+         'audio_timestamp': [94728, 96742]},
+        {'timestamp': [49.24, 52.2], 'text': " If it's very low in the sky, I might do that",
+         'audio_timestamp': [99562, 101342]},
+        {'timestamp': [52.2, 54.52], 'text': " because it's not going to be very painful to my eyes.",
+         'audio_timestamp': [104873, 107340]},
+        {'timestamp': [54.52, 56.84], 'text': ' However, if the sun is a little bit brighter.',
+         'audio_timestamp': [110947, 112718]}]
     video_path = "07fsfECkwma6fVTDyqQf.mp4"
     audio_path = "07fsfECkwma6fVTDyqQf-translated.mp3"
     project_id = "07fsfECkwma6fVTDyqQf"
