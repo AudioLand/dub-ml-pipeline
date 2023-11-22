a31f7499308081adc0bf7ed58a14f51e5c89881f
Syrnnik <sakh.alexandr@gmail.com>
2023-11-22 20:46:29 +0300

    fix: use generate controller in app

--- src/main.py
+++ src/main.py
@@ -1,20 +1,10 @@
-# from integrations.youtube_utils import youtube_download
-
 from fastapi import FastAPI
 
-# from gender_detection import voice_gender_detection
+from controllers.generate import dub_router
 
 app = FastAPI()
 
-
-# youtube_link = "https://youtu.be/WDv4AWk0J3U?si=wL3cKW1PCvinxBDy"
-# video_path = 'test-video-1min.mp4'
-
-# source_blob_name = 'XYClUMP7wEPl8ktysClADpuaPIq2/4kIRz5B1JY0GAO1uj0dE/test-video-1min.mp4'
-
-# useId, projectId,
-
-# https://audioland.fly.dev/?original_file_location=XYClUMP7wEPl8ktysClADpuaPIq2/4kIRz5B1JY0GAO1uj0dE/test-video-1min.mp4
+app.include_router(dub_router)
 
 
 @app.get("/healthcheck")
