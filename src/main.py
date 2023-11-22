339f7284354d72dafa1c0ab037d608879bcb6b00
Syrnnik <sakh.alexandr@gmail.com>
2023-11-22 21:51:17 +0300

    fix: return previous port

--- src/main.py
+++ src/main.py
@@ -15,4 +15,4 @@ def health_check():
 
 if __name__ == "__main__":
     print("main started")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
